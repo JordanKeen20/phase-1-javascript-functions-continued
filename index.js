31b6f5d29c1e30dcad5c70009859e88b419d8f56
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-01 04:12:39 -0700

    commit tree e659df2e712ef202486be6d6624ec01667963ba4

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity){
+    if (activity ==='bathe my dog'){
+        return 'This Saturday, I want to bathe my dog!'
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+    
+}
+
+
+function mondayWork(responsibility){
+    if (responsibility === 'work from home'){
+        return 'This Monday, I will work from home.'
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+
+function wrapAdjective(odd='*'){
+    return function(character = 'special') {
+        return `You are ${odd}${character}${odd}!`
+    }
+}
+    
